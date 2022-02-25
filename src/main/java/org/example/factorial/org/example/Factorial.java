3b739378db5ec192904f9d2588291ebd5b7e0bf0
Dariiopk <dariogarciamartin@uma.es>
2022-02-25 11:51:05 +0100

    Pruebas hasta 4 negativo y Factorial implementado

--- src/main/java/org/example/factorial/org/example/Factorial.java
+++ src/main/java/org/example/factorial/org/example/Factorial.java
@@ -3,6 +3,7 @@ package org.example.factorial.org.example;
 public class Factorial {
 
     public static int compute(int value) {
+        //Nueva rama en develop wow
         if (value < 0) {
             throw new RuntimeException("Números negativos no válidos");
         }
